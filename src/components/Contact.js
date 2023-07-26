3d2a27d9b61e278bba7959242c84408b0f8e0339
Bruno_MH <brunomh141a@gmail.com>
2023-07-25 21:41:30 -0600

    react-router

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = (props ) => {
+    return(
+        <div 
+        className="ui raised very padded tect container segment"
+        style={{marginTop:'80px'}}
+        >
+        <h3 className="ui header">Contact</h3>
+        <p> Lorem ipsum dolor sit amet, consectetur adipsing elit,se do eiou</p>
+
+        </div>
+    )
+}
+
+export default Contact;
